ceb1e08623210ae847e189a1ab6bddd2498dad64
xdenote <xdenote@gmail.com>
2023-03-24 22:11:38 +0500

    build a navbar

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
                     lg: `"nav nav" "aside main"`,
                 }}
             >
-                <GridItem area="nav" bg="lightcoral">
+                <GridItem area="nav">
                     <NavBar/>
                 </GridItem>
                 <Show above="lg">
