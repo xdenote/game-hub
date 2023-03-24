11e9d6aaa3f27ef56b8ea6a41bd16c8abdbad68c
xdenote <xdenote@gmail.com>
2023-03-24 22:10:38 +0500

    uninstall packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import {ChakraProvider, Grid, GridItem, Show} from '@chakra-ui/react'
+import NavBar from "./components/NavBar";
 
 function App() {
     return (
@@ -11,7 +12,9 @@ function App() {
                     lg: `"nav nav" "aside main"`,
                 }}
             >
-                <GridItem area="nav" bg="lightcoral">Nav</GridItem>
+                <GridItem area="nav" bg="lightcoral">
+                    <NavBar/>
+                </GridItem>
                 <Show above="lg">
                     <GridItem area="aside" bg="gold">Aside</GridItem>
                 </Show>
