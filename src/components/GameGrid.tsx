e62eeb304c74e7c5b468b4444902d5c0e4153c9d
xdenote <xdenote@gmail.com>
2023-03-25 17:08:53 +0500

    Remove duplicate codes

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -3,6 +3,7 @@ import {SimpleGrid, Text} from "@chakra-ui/react";
 import useGames from "../hooka/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
+import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
     const {games, error, isLoading } = useGames();
@@ -11,9 +12,16 @@ const GameGrid = () => {
         <>
         {error && <Text>{error}</Text>}
   <SimpleGrid columns={{sm:1, md: 2, lg: 3, xl: 5}} padding={10} spacing={10}>
-      {isLoading && skeletons.map(skeleton => <GameCardSkeleton key={skeleton}/>)}
+      {isLoading && skeletons.map((skeleton) => (
+          <GameCardContainer>
+              <GameCardSkeleton key={skeleton} />
+          </GameCardContainer>
+          ))}
       {games.map((game) => (
-          <GameCard key={game.id} game={game}/>))}
+          <GameCardContainer>
+              <GameCard key={game.id} game={game}/>
+          </GameCardContainer>
+      ))}
   </SimpleGrid>
         </>
  )};
