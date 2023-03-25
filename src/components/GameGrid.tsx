08ceb574f3dab9e75d5b35a1014e5ebe87dc93b9
xdenote <xdenote@gmail.com>
2023-03-25 16:51:09 +0500

    Game Card Skeleton

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 import {SimpleGrid, Text} from "@chakra-ui/react";
 import useGames from "../hooka/useGames";
 import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 const GameGrid = () => {
-    const {games, error} = useGames();
+    const {games, error, isLoading } = useGames();
+    const skeletons = [1, 2, 3, 4, 5, 6];
     return (
         <>
         {error && <Text>{error}</Text>}
   <SimpleGrid columns={{sm:1, md: 2, lg: 3, xl: 5}} padding={10} spacing={10}>
+      {isLoading && skeletons.map(skeleton => <GameCardSkeleton key={skeleton}/>)}
       {games.map((game) => (
-          <GameCard key={game.id} game={game}/> ))}
+          <GameCard key={game.id} game={game}/>))}
   </SimpleGrid>
         </>
  )};
